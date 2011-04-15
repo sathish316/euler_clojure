1073c40240d52c63f394f65ba2e428d63e58552f
sathish <sahthish316@gmail.com>
2011-04-15 08:59:36 +0530

    #6 sigma delta

--- src/P006_sum_of_squares.clj
+++ src/P006_sum_of_squares.clj
@@ -1,1 +1,17 @@
+(defn square [x] (* x x))
 
+(defn one-to-n [n] (range 1 (inc n)))
+
+(defn sigma_x2 [n]
+  (reduce + (map square (one-to-n n))))
+
+(defn sigmax_2 [n]
+  (square (reduce + (one-to-n n))))
+
+(defn sigma_delta [n]
+  (- (sigmax_2 n) (sigma_x2 n)))
+
+(println (sigma_x2 10))
+(println (sigmax_2 10))
+(println (sigma_delta 10))
+(println (sigma_delta 100))
